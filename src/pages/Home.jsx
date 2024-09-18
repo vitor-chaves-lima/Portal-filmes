a97426aa781ca72f3c16b1372b9403ec89ba25d8
Vitor Coelho <vitor.coelho@skf.com>
2024-09-18 20:55:54 -0300

    Adicionando listagem de filmes mais avaliados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,22 @@ import movies from "../data/movies.json";
 
 export default function Home() {
   return (
-    <CardContainer title={"Filmes antigos"}>
-      {movies
-        .filter((m) => m.ano_lancamento < 2000)
-        .map((m, i) => (
-          <MovieCard {...m} key={i} />
-        ))}
-    </CardContainer>
+    <div className="flex flex-col gap-6">
+      <CardContainer title={"Filmes antigos"}>
+        {movies
+          .filter((m) => m.ano_lancamento < 2000)
+          .map((m, i) => (
+            <MovieCard {...m} key={i} />
+          ))}
+      </CardContainer>
+
+      <CardContainer title={"Mais avaliados"}>
+        {movies
+          .filter((m) => m.avaliacao > 8.5)
+          .map((m, i) => (
+            <MovieCard {...m} key={i} />
+          ))}
+      </CardContainer>
+    </div>
   );
 }
